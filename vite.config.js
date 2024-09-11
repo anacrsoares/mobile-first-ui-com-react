6d22b840bf26f61d880657597ed4eb0192d70ad9
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-09-11 20:12:17 -0300

    Atualizando arquivos da branch principal para gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: process.env.VITE_BASE_PATH,
-=======
   base: "",
->>>>>>> master
   plugins: [react()],
 });
