62f3149508279820d67f68dbcb5fd7e0a9d2b0cd
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-09-11 14:34:52 -0300

    Commit gh pages revert: 2024-09-11

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
 export default function App() {
   return (
     <main>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/src/TP2/01/index.html">TP 2.1</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/02/index.html">TP 2.2</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/03/index.html">TP 2.3</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/04/index.html">TP 2.4</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/05/index.html">TP 2.5</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/06/index.html">TP 2.6</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/07/index.html">TP 2.7</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/08/index.html">TP 2.8</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/09/index.html">TP 2.9</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/10/index.html">TP 2.10</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/11/index.html">TP 2.11</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/12/index.html">TP 2.12</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/13/index.html">TP 2.13</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/14/index.html">TP 2.14</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/15/index.html">TP 2.15</Link>
-          </li>
-          <li>
-            <Link to="/src/TP2/16/index.html">TP 2.16</Link>
-          </li>
-        </ul>
-      </nav>
+      <a href="./src/TP2/01/index.html">TP 2.1</a>
+      <a href="./src/TP2/02/index.html">TP 2.2</a>
+      <a href="./src/TP2/03/index.html">TP 2.3</a>
+      <a href="./src/TP2/04/index.html">TP 2.4</a>
+      <a href="./src/TP2/05/index.html">TP 2.5</a>
+      <a href="./src/TP2/06/index.html">TP 2.6</a>
+      <a href="./src/TP2/07/index.html">TP 2.7</a>
+      <a href="./src/TP2/08/index.html">TP 2.8</a>
+      <a href="./src/TP2/09/index.html">TP 2.9</a>
+      <a href="./src/TP2/10/index.html">TP 2.10</a>
+      <a href="./src/TP2/11/index.html">TP 2.11</a>
+      <a href="./src/TP2/12/index.html">TP 2.12</a>
+      <a href="./src/TP2/13/index.html">TP 2.13</a>
+      <a href="./src/TP2/14/index.html">TP 2.14</a>
+      <a href="./src/TP2/15/index.html">TP 2.15</a>
+      <a href="./src/TP2/16/index.html">TP 2.16</a>
     </main>
   );
 }
-
-// export default function App() {
-//   return (
-//     <main>
-//       <a href="./src/TP2/01/index.html">TP 2.1</a>
-//       <a href="./src/TP2/02/index.html">TP 2.2</a>
-//       <a href="./src/TP2/03/index.html">TP 2.3</a>
-//       <a href="./src/TP2/04/index.html">TP 2.4</a>
-//       <a href="./src/TP2/05/index.html">TP 2.5</a>
-//       <a href="./src/TP2/06/index.html">TP 2.6</a>
-//       <a href="./src/TP2/07/index.html">TP 2.7</a>
-//       <a href="./src/TP2/08/index.html">TP 2.8</a>
-//       <a href="./src/TP2/09/index.html">TP 2.9</a>
-//       <a href="./src/TP2/10/index.html">TP 2.10</a>
-//       <a href="./src/TP2/11/index.html">TP 2.11</a>
-//       <a href="./src/TP2/12/index.html">TP 2.12</a>
-//       <a href="./src/TP2/13/index.html">TP 2.13</a>
-//       <a href="./src/TP2/14/index.html">TP 2.14</a>
-//       <a href="./src/TP2/15/index.html">TP 2.15</a>
-//       <a href="./src/TP2/16/index.html">TP 2.16</a>
-//     </main>
-//   );
-// }
