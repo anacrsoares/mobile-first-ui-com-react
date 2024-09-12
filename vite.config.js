3983d151a1a74af4a1b3bf55549e57a129b504c9
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-09-11 21:02:18 -0300

    Atualizando vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mobile-first-ui-com-react/",
+  base: "/mobile-first-ui-com-react/src/",
   plugins: [react()],
 });
